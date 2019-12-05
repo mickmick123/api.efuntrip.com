0d211bd4df50a00d8126841d6bc052b433bb2897
marccorpus <mario.corpus656@gmail.com>
2019-12-05 09:55:32 +0800

    Delete files

--- config/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'domain'=>'http://api.com',
-    //passport配置
-    'grant_type' => env('OAUTH_GRANT_TYPE'),
-    'client_id' => env('OAUTH_CLIENT_ID'),
-    'client_secret' => env('OAUTH_CLIENT_SECRET'),
-    'scope' => env('OAUTH_SCOPE', '*'),
-];
