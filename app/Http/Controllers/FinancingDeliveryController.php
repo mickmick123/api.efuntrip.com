0e73448c7e01e6d7265c76c804fe4185b7a8105e
jefree656 <jeffrey.gatpandan656@gmail.com>
2020-04-20 15:04:48 +0800

    no message

--- app/Http/Controllers/FinancingDeliveryController.php
+++ app/Http/Controllers/FinancingDeliveryController.php
@@ -191,7 +191,7 @@ class FinancingDeliveryController extends Controller
       }else{
         if($update_field == 'add_purchasing_budget'){
           $f = FinancingDelivery::where('id', intval($request->finance_id))->first();
-          $f->trans_desc = $f->trans_desc.'<br>&bull; Additional Purchasing Budget : '.$request->amount;
+          $f->trans_desc = $f->trans_desc.'<br>&bull; '.$request->req_user.' additional purchasing budget : '.$request->amount;
           $f->purchasing_budget = $f->purchasing_budget + $request->amount;
           $f->save();
         }
