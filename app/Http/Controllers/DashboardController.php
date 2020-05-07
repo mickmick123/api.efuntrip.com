bd5a9550c4826f7fa7bb58ab3195f8dd966d48f9
jefree656 <jeffrey.gatpandan656@gmail.com>
2020-05-07 12:20:04 +0800

    fix dashboard after transferring client services from old live visa site

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,6 +9,7 @@ use App\ClientService;
 use App\Role;
 
 use App\Branch;
+use App\BranchUser;
 
 use Carbon\Carbon;
 
@@ -20,8 +21,9 @@ class DashboardController extends Controller
 {
 	//get branch of current user
 	public function getBranchAuth(){
-		$branch = DB::table('branch_user')->where('user_id', Auth::User()->id)
-                ->pluck('branch_id')[0];
+		$branch = BranchUser::where('user_id', Auth::User()->id)
+                ->pluck("branch_id");
+        // \Log::info($branch);
         return $branch;
 	}
 
@@ -41,12 +43,12 @@ class DashboardController extends Controller
         //determine services dated today and yesterday
         $today_services = ClientService::whereDate('created_at', $today)->where('active',1)
                           ->whereHas('client.branches', function ($query) use ($auth_branch) {
-                                $query->where('branches.id', '=', $auth_branch);
+                                $query->whereIn('branches.id', $auth_branch);
                           });
 
         $yesterday_services = ClientService::whereDate('created_at', $yesterday)->where('active',1)
                           ->whereHas('client.branches', function ($query) use ($auth_branch) {
-                                $query->where('branches.id', '=', $auth_branch);
+                                $query->whereIn('branches.id', $auth_branch);
                           });
 
         //get today and yesterday services
@@ -79,11 +81,12 @@ class DashboardController extends Controller
 	 	$totals['total_clients'] = User::whereHas('roles', function ($query) use ($role_id) {
                 $query->where('roles.id', '=', $role_id);
 	 	 	 	})->whereHas('branches', function ($query) use ($auth_branch) {
-                $query->where('branches.id', '=', $auth_branch);
+                $query->whereIn('branches.id', $auth_branch);
 	 	 	 	})->count();
-	 	$totals['total_services'] = ClientService::whereHas('client.branches', function ($query) use ($auth_branch) {
-                $query->where('branches.id', '=', $auth_branch);
-	 	 	 	})->where('active',1)->count();
+        $clients = User::whereHas('branches', function ($query) use ($auth_branch) {
+                $query->whereIn('branches.id', $auth_branch);
+                })->pluck('id');
+	 	$totals['total_services'] = ClientService::whereIn('client_id',$clients)->where('active',1)->count();
 	 	$totals['total_services_today'] = $today_services->count();
 	 	$totals['total_services_yesterday'] = $yesterday_services->count();
         $totals['total_services_cost_today'] = $total_cost_today;
