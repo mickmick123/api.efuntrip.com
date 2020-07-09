61deeb1131a15e501a2d437866b17f9e6b7b2f0c
Joshua Alexis Sardido <joshuaalexis.sardido656@gmail.com>
2020-07-09 15:57:22 +0800

    Inventory Models

---
+++ app/Inventory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $table = 'inventory';
+    public $primaryKey  = 'inventory_id';
+    public $timestamps = false;
+
+    protected $fillable = ['category_id', 'inventory_name','inventory_name_chinese','inventory_price','orig_price','unit','multiplier', 'inventory_img', 'inventory_description', 'status'];
+}
