86f721f0d35fa47699a2bb51866308d45aa81228
Macoy Bayona <markanthony.bayona@gmail.com>
2020-09-18 15:18:29 +0800

    Update v1

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3596,15 +3596,16 @@ public function getClientPackagesByGroup($client_id, $group_id){
         $members = [];
         $r['members'] = collect($r['members'])->reverse()->toArray();
         foreach($r['members'] as $member){
-            $member['services'] = collect($member['services'])->sortBy('total_service_cost')->toArray();
+      //      $member['services'] = collect($member['services'])->sortBy('total_service_cost')->toArray();
             $members[$j] = $member;
             $j++;
         }
         $result2[$ctr]['members'] = $members;
         $ctr++;
     }
-    $result2 = collect($response)->sortBy('sdate')->reverse()->toArray();
 
+    $result2 = collect($response)->sortBy('sdate')->reverse()->toArray();
+    //$result2 = collect($result2)->reverse()->toArray();
 
     return [
         'services' => $result2,
