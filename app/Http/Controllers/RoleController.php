1b1dfdefa6357da0cd4efdb0481335583a8304d7
Joshua Alexis Sardido <joshuaalexis.sardido656@gmail.com>
2020-09-22 13:38:16 +0800

    Roles Api

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Role;
-
+use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RoleController extends Controller
 {
@@ -13,4 +14,36 @@ class RoleController extends Controller
 
         return $roles;
 	}
+
+	public function addRole(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+        if($validator->fails()) {
+            $response['status'] = 'Failed';
+            $response['errors'] = $validator->errors();
+            $response['code'] = 422;
+        } else {
+            $response['status'] = 'Success';
+            $response['code'] = 200;
+            $response['data'] = 'Role has been Added '.str_slug($request->name);
+        }
+        return Response::json($response);
+    }
+
+    public function getRole(Request $request, $perPage = 10) {
+        $sort = $request->sort;
+        $search = $request->search;
+
+        $list = Role::where('name','LIKE',$search)
+            ->when($sort != '', function ($q) use($sort){
+                $sort = explode('-' , $sort);
+                return $q->orderBy($sort[0], $sort[1]);
+            })->paginate($perPage);
+
+        $response['status'] = 'Success';
+        $response['code'] = 200;
+        $response['data'] = $list;
+        return Response::json($response);
+    }
 }
--- routes/api/role.php
+++ routes/api/role.php
@@ -5,4 +5,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->group(function() {
 
 	Route::get('/', 'RoleController@index');
+
+    Route::get('get-role/{perPage?}', 'RoleController@getRole');
+    Route::post('add-role', 'RoleController@addRole');
 });
