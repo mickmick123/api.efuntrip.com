c116f70f0bd15de2e2c5fd8ee7d7f98301920e20
jefree656 <jeffrey.gatpandan656@gmail.com>
2020-10-14 14:30:10 +0800

    no message

--- app/OnHandDocument.php
+++ app/OnHandDocument.php
@@ -13,7 +13,7 @@ class OnHandDocument extends Model
     
 	protected $table = 'on_hand_documents';
 
-    protected $fillable = ['client_id', 'document_id', 'count', 'processor_id', 'sender_id'];
+    protected $fillable = ['client_id', 'document_id', 'count', 'processor_id', 'sender_id', 'employee_id'];
 
     public function client() {
     	return $this->belongsTo('App\User', 'client_id', 'id');
--- app/Tasks.php
+++ app/Tasks.php
@@ -9,7 +9,7 @@ class Tasks extends Model
     
     protected $table = 'tasks';
 
-    protected $fillable = ['id', 'client_service_id', 'who_is_in_charge', 'where_to_file', 'estimated_cost', 'status', 'date', 'custom_date'];
+    protected $fillable = ['id', 'client_service_id', 'who_is_in_charge', 'date'];
 
     public function client_service() {
     	return $this->hasOne('App\ClientService', 'id', 'client_service_id');
