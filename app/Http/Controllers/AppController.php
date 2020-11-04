3c54cfcad6080a3f9ef641fdb74293fdf945b14f
jefree656 <jeffrey.gatpandan656@gmail.com>
2020-11-04 14:38:39 +0800

    no message

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -434,6 +434,7 @@ class AppController extends Controller
                 ]
             );
             // return $r;
+            \Log::info($r);
             $r = json_decode($r->getBody(), true);
             return Redirect::to($r['data']['content']);
         }
