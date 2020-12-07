282cb4d340e9123f4a8ee7833c69a67723a43ba6
ariel656 <ayeh.aquino656@gmail.com>
2020-12-07 11:40:52 +0800

    refactor

--- app/Helpers/MessageHelper.php
+++ app/Helpers/MessageHelper.php
@@ -4,45 +4,38 @@ namespace App\Helpers;
 
 class MessageHelper
 {
-    public static function MsgNotification($type, $date=null, $option1=null, $option2=null, $option3=null, $option4=null)
-    {
-        $msg = '';
-
-        switch ($type) {
-            case "E-wallet Deposit":
-                $msg =  "Your e-wallet deposit amounting to $option1 on $date has been added to your account. Your new balance is $option2";
-                break;
-
-            case "Documents Released":
-                $msg =  "Your documents have been released on $date You can now view it on your account.";
-                break;
-
-            case "Documents Received":
-                $msg =  "Your documents have been received by $option1 on $date you can now view it on your account.";
-                break;
-
-            case ("Withdrawal" || "Service Payment 1"):
-                $type = "withdrawal";
-                if($type != "Withdrawal"){
-                    $type = "payment";
-                }
-                $msg =  "Your $type amounting to $option1 has been successfully processed on $date";
-                break;
-
-            case "Service Payment 2":
-                $msg =  "Your payment amounting to $option1 through $option2 has been successfully processed on $date.";
-                break;
-
-            case "Service Payment 3":
-                $msg =  "$option1".PHP_EOL."Paid total amount of $option2 to service $option3".PHP_EOL.$option4;
-                break;
-
-            case "Added Service":
-                $msg =  "$option1".PHP_EOL."$option2".PHP_EOL."Package $option3 with the Total Service Cost of $option4";
-                break;
+    public static function MsgNotification($type, $data=[]) {
+        $msg = "";
+        if ($type == "E-wallet Deposit") {
+            $msg = "Your e-wallet deposit amounting to ".$data['amount']." on ".$data['date']." has been added to your account. Your new balance is". $data['balance'];
+        }
+        if ($type == "Documents Released") {
+            $msg = "Your documents have been released on ".$data['date']." You can now view it on your account.";
+        }
+        if ($type == "Documents Received"){
+            $msg = "Your documents have been received by ". $data['user'] ." on ".$data['date']." you can now view it on your account.";
+        }
+        if($type == "Withdrawal" || $type == "Service Payment 1") {
+            $type = "withdrawal";
+            if ($type != "Withdrawal") {
+                $type = "payment";
+            }
+            $msg = "Your $type amounting to ".$data['amount']." has been successfully processed on ".$data['date'];
+        }
+        if($type == "Service Payment 2") {
+            $msg = "Your payment amounting to ".$data['amount']." through ".$data['$data->bank']." has been successfully processed on ".$data['date'].".";
+        }
+        if($type == "Service Payment 3") {
+            $msg = $data['clients']. PHP_EOL . "Paid total amount of ".$data['amount']." to service ".$data['duration'] . PHP_EOL . $data['service'];
+        }
+        if($type == "Added Service") {
+            $x = "";
+            if ($data['clients'] && $data['clients'] != null) {
+                $x = $data['client'] . PHP_EOL;
+            }
+            $msg = $x . $data['service'] . PHP_EOL . "Package ". $data['package'] ." with the Total Service Cost of ".$data['amount'];
         }
 
         return $msg;
     }
-
 }
