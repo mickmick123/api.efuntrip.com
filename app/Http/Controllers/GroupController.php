6e254bbbe6d38475a413fa64f6216adb59f19fde
Herbert Asis <herbert.asis@collabera.com>
2022-07-20 15:21:51 +0800

    no message

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3666,8 +3666,8 @@ public function getClientPackagesByGroup($client_id, $group_id){
 
 
     public function getClientServiceSummary(Request $request) {
-        $export = new ServiceByClient('EN', $request->data['services'], $request->data['user']);
-        return Excel::download($export, 'xxxx.xlsx');
+        // $export = new ServiceByClient('EN', $request->data['services'], $request->data['user']);
+        return Excel::download({}, 'xxxx.xlsx');
     }
 
     public function getGroupSummary(Request $request){
